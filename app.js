c8a9898378a78badd928c5a5094a6fd37ffc01d9
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-16 18:03:28 +0530

    login and registration page with cart page layouts

--- app.js
+++ app.js
@@ -8,11 +8,22 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-// app.use(expressLayout);
+app.use(expressLayout);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
 });
 
 module.exports = app;
