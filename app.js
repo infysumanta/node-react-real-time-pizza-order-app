a0a2bdc2c7aacad5b0718f0b2fe847a4d4017ab8
Sumanta Kabiraj <sumantablog@gmail.com>
2022-12-16 15:34:51 +0530

    basic project setup with express server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.use(expressLayout);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+module.exports = app;
